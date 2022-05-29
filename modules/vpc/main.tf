2bde50775cc96d79709c8168569ea45c9a51bc4e
Bruno Souto <bcovies@gmail.com>
2022-05-28 21:34:08 -0300

    Fix ports sg

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -22,15 +22,15 @@ resource "aws_security_group" "vpc_internal_sg" {
   ingress {
     description      = "Allows any protocol for internal use"
     from_port        = 0
-    to_port          = 65535
-    protocol         = "tcp"
+    to_port          = 0
+    protocol         = "-1"
     cidr_blocks      = [aws_vpc.vpc.cidr_block]
   }
 
   egress {
     description      = "Allows any protocol to internet"
     from_port        = 0
-    to_port          = 65535
+    to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
